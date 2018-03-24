3ab538129b944486c8f99e52471f2db4065deac3
agrmv <gromov-aleksey@outlook.com>
2018-03-24 20:29:57 +0700

    Release :eyes:

--- Lagrange/main.cpp
+++ Lagrange/main.cpp
@@ -2,45 +2,42 @@
 #include <iostream>
 #include <algorithm>
 #include <GL/glut.h>
-#include <cmath>
-#include <iostream>
 #include <vector>
-#include <stdlib.h>
 #include <string>
 #include <sstream>
 
 using vector_t = std::vector<double>;
-extern const int16_t xMin = -2;
-extern const int16_t xMax =  2;
-extern const int16_t yMin = -2;
-extern const int16_t yMax =  2;
-extern const int16_t leftmost  = -1;
-extern const int16_t rightmost =  1;
-extern const int16_t uppermost =  1;
-extern const int16_t lowermost = -1;
-extern const double step 	   = 0.5;
+static const int16_t xMin = -2;
+static const int16_t xMax =  2;
+static const int16_t yMin = -2;
+static const int16_t yMax =  2;
+static const double step  =  0.5;
 
 
-void draw_string_bitmap(void *font, const char* string) 
-{
-  while (*string)
-    glutBitmapCharacter(font, *string++);
+void drawStringBitmap(void *font, const char* string) {
+	while (*string) {
+		glutBitmapCharacter(font, *string++);
+	}
 }
 
 double q(const vector_t &vec, double x, size_t n) {
-    if (n >= vec.size())
+    if (n >= vec.size()) {
         return 1;
+    }
     double result = 1;
-    for (size_t i = 0; i < vec.size(); ++i)
-        if (i != n)
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (i != n) {
             result *= (x - vec[i]) / (vec[n] - vec[i]);
+        }
+    }
     return result;
 }
 
 double p(const vector_t &_x, const vector_t &y, double x, size_t n) {
     double res = 0;
-    for (size_t i = 0; i <= n; ++i)
+    for (size_t i = 0; i <= n; ++i) {
         res += y[i] * q(_x, x, i);
+    }
     return res;
 }
 
@@ -54,10 +51,10 @@ void display() {
         std::cin >> _x[i];
         std::cout << "Enter y" << i << ": ";
         std::cin >> y[i];
-    }*/
-    size_t n = 4;
+    }*/  
     vector_t _x{-1.0, -0.6, -0.3, 0.3, 1.0};
     vector_t  y{ 0.5,  1.0,  0.4, 0.1, 0.5};
+    size_t n = _x.size();
     
     glClear(GL_COLOR_BUFFER_BIT);   
     glColor3f(0.0, 0.0, 0.0);
@@ -65,43 +62,52 @@ void display() {
     glBegin(GL_LINES);
 		glVertex2f(xMin, 0);   
 		glVertex2f(xMax, 0); 
-		for (double i = leftmost; i <= rightmost; i += step) {
+		for (double i = xMin; i <= xMax; i += step) {
 			glVertex2f(-0.05, i);
 			glVertex2f( 0.05, i);
 		} 
     glEnd();
     
-    for (double i = leftmost; i <= rightmost; i += step) {
+    for (double i = xMin; i <= xMax; i += step) {
 		glRasterPos2f(i, -0.2);
 		std::ostringstream ch;
 		ch << i;
-		draw_string_bitmap(GLUT_BITMAP_HELVETICA_18, ch.str().c_str());
+		drawStringBitmap(GLUT_BITMAP_HELVETICA_18, ch.str().c_str());
 	}
     //OY
     glBegin(GL_LINES);
 		glVertex2f(0, yMin);
 		glVertex2f(0, yMax);
-		for (double i = leftmost; i <= rightmost; i += step) {
+		for (double i = yMin; i <= yMax; i += step) {
 			glVertex2f(i, -0.05);
 			glVertex2f(i,  0.05);
 		} 
     glEnd();
 	
-	for (double i = leftmost; i <= rightmost; i += step) {
+	for (double i = yMin; i <= yMax; i += step) {
 		if (i == 0) continue;
 		glRasterPos2f(0.1, i);
 		std::ostringstream ch;
 		ch << i;
-		draw_string_bitmap(GLUT_BITMAP_HELVETICA_18, ch.str().c_str());
+		drawStringBitmap(GLUT_BITMAP_HELVETICA_18, ch.str().c_str());
 	}
+	
+	glPointSize(8);
+	glEnable(GL_POINT_SMOOTH);
+	glBegin(GL_POINTS);
+		glColor3f(1.0,0.0,0.0);
+		for (auto &i: _x) {
+				glVertex2d(i, p(_x, y, i, n));
+		}
+	glEnd();
     
     glBegin(GL_LINE_STRIP);  
-    glColor3f(1.0,0.0,0.0);
-    auto leftX  = *std::min_element(_x.begin(), _x.end()); 
-    auto rightX = *std::max_element(_x.begin(), _x.end()); 
-    for (double i = leftX; i < rightX; i += 0.001) { 
-        glVertex2d(i, p(_x, y, i, n));
-    } 
+		glColor3f(1.0,0.0,0.0);
+		auto leftX  = *std::min_element(_x.begin(), _x.end()); 
+		auto rightX = *std::max_element(_x.begin(), _x.end()); 
+		for (double i = leftX; i < rightX; i += 0.001) { 
+			glVertex2d(i, p(_x, y, i, n));
+		} 
     glEnd();
     glFlush();
 }
@@ -115,7 +121,7 @@ int main(int argc, char** argv) {
     glClearColor(1.0 , 1.0 , 1.0 , 1.0); 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(xMin, xMax, yMin, yMax); 
+    gluOrtho2D(xMin - 0.2, xMax + 0.2, yMin - 0.2, yMax + 0.2); 
     glutDisplayFunc(display); 
     glutMainLoop(); 
     return 0;
