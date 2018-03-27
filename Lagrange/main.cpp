49ed5e90c8631cee498017885539c6802fdb7de7
agrmv <gromov-aleksey@outlook.com>
2018-03-27 17:51:26 +0700

    glVertex2f to glVertex2d

--- Lagrange/main.cpp
+++ Lagrange/main.cpp
@@ -1,9 +1,7 @@
-//g++  -std=c++0x main.cpp -lglut -lGL -lGLU -lGLEW
 #include <iostream>
 #include <algorithm>
 #include <GL/glut.h>
 #include <vector>
-#include <string>
 #include <sstream>
 
 using vector_t = std::vector<double>;
@@ -45,34 +43,34 @@ void drawGrid() {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0.0, 0.0, 0.0);
     //OX
-    glBegin(GL_LINES); 
-        glVertex2f(xMin, 0);
-        glVertex2f(xMax, 0);
+    glBegin(GL_LINES);
+        glVertex2d(xMin, 0);
+        glVertex2d(xMax, 0);
         for (double i = xMin; i <= xMax; i += step) {
-	    glVertex2f(-0.05, i);
-            glVertex2f( 0.05, i);
+	    glVertex2d(-0.05, i);
+            glVertex2d( 0.05, i);
         }
     glEnd();
-    
+
     for (double i = xMin; i <= xMax; i += step) {
-        glRasterPos2f(i, -0.2);
+        glRasterPos2d(i, -0.2);
         std::ostringstream ch;
         ch << i;
         drawStringBitmap(GLUT_BITMAP_HELVETICA_18, ch.str().c_str());
     }
     //OY
     glBegin(GL_LINES);
-        glVertex2f(0, yMin);
-        glVertex2f(0, yMax);
+        glVertex2d(0, yMin);
+        glVertex2d(0, yMax);
         for (double i = yMin; i <= yMax; i += step) {
-            glVertex2f(i, -0.05);
-            glVertex2f(i,  0.05);
+            glVertex2d(i, -0.05);
+            glVertex2d(i,  0.05);
         }
     glEnd();
 
     for (double i = yMin; i <= yMax; i += step) {
         if (i == 0) continue;
-        glRasterPos2f(0.1, i);
+        glRasterPos2d(0.1, i);
         std::ostringstream ch;
         ch << i;
         drawStringBitmap(GLUT_BITMAP_HELVETICA_18, ch.str().c_str());
@@ -89,13 +87,13 @@ void display() {
         std::cin >> _x[i];
         std::cout << "Enter y" << i << ": ";
         std::cin >> y[i];
-    } */ 
+    } */
     vector_t _x{-1.0, -0.6, -0.3, 0.3, 1.0};
     vector_t  y{ 0.5,  1.0,  0.4, 0.1, 0.5};
     if (_x.size() != y.size()) {
 		throw std::logic_error("Error vector's size");
 	}
-    size_t n = _x.size(); 
+    size_t n = _x.size();
 
     glPointSize(8);
     glEnable(GL_POINT_SMOOTH);
@@ -118,17 +116,17 @@ void display() {
 }
 
 int main(int argc, char** argv) {
-    glutInit(&argc,argv); 
+    glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(800, 800); 
-    glutInitWindowPosition(0,0);  
-    glutCreateWindow("LAGRANGE"); 
-    glClearColor(1.0 , 1.0 , 1.0 , 1.0); 
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("LAGRANGE");
+    glClearColor(1.0 , 1.0 , 1.0 , 1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(xMin - 0.2, xMax + 0.2, yMin - 0.2, yMax + 0.2);
     drawGrid();
-    glutDisplayFunc(display); 
-    glutMainLoop(); 
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
